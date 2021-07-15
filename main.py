6be5c89f12a3d2e26c043ade1964c3d6b4c9aa54
Santiago A <sarmstrong@uc.cl>
2021-07-15 00:32:58 -0400

    updated var names

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import numpy as np
 import pandas as pd
 
 
+class Square:
+	"""
+	Object used to represent one squared tile of the puzzle. 
 
-cardinal_dirs = [ np.asarray(tup) for tup in [(1,0), (-1,0), (0,1),(0,-1)]]
+	Each Square has one label in each of its four sides. This labels are stored in a list of strings
 
-class Car:
+	"""
 	def __init__(self, borders, id="0"):
 		self.borders = borders #N W S E
 		self.id = id
@@ -27,20 +30,20 @@ class Car:
 		"""permute cyclically the labels of the sides"""
 		self.borders = [self.W, self.S, self.E, self.N]   
 		
-	def compare(self, other_car, cardinal_dir):
+	def compare(self, other_square, cardinal_dir):
 		cardinal_dir = tuple(cardinal_dir)
 		if cardinal_dir == (1,0):
 			# tengo que comparar mi S con tu N
-			return (self.S[0] == other_car.N[0]) and (self.S[1] != other_car.N[1]) 
+			return (self.S[0] == other_square.N[0]) and (self.S[1] != other_square.N[1]) 
 		elif cardinal_dir == (-1, 0):
 			# tengo que comparar mi N con tu S
-			return (self.N[0] == other_car.S[0]) and (self.N[1] != other_car.S[1])
+			return (self.N[0] == other_square.S[0]) and (self.N[1] != other_square.S[1])
 		elif cardinal_dir == (0,1):
 			# tengo que comparar mi E con tu W
-			return (self.E[0] == other_car.W[0]) and (self.E[1] != other_car.W[1]) 
+			return (self.E[0] == other_square.W[0]) and (self.E[1] != other_square.W[1]) 
 		elif cardinal_dir == (0,-1):
 			# tengo que comparar mi W con tu E
-			return (self.W[0] == other_car.E[0]) and (self.W[1] != other_car.E[1])
+			return (self.W[0] == other_square.E[0]) and (self.W[1] != other_square.E[1])
 
 		else:
 			raise Exception
@@ -58,13 +61,13 @@ class Board:
 	def __init__(self, shape):
 		self.array = np.array([None for i in range(shape[0]*shape[1])]).reshape(shape)
 		self.shape = shape
-		self.list_pos = [(i,j) for i in range(self.shape[0]) for j in range(self.shape[1])]
-
 	
 
 	def get_adjacent_indices(self, current_pos):
 		# returns list of tuples of indices which ...
-		neighbours = [] 
+		neighbours = []
+		cardinal_dirs = [ np.asarray(tup) for tup in [(1,0), (-1,0), (0,1),(0,-1)]]
+
 		for dir in cardinal_dirs:
 			neighbour = np.asarray(current_pos) + dir # Northern, Southern, Easter and western neighbour
 			if 0<= neighbour[0] < self.shape[0] and 0<= neighbour[1] < self.shape[1]:
@@ -72,12 +75,12 @@ class Board:
 				neighbours.append((tuple(neighbour), dir))
 		return neighbours
 
-	def is_compatible(self, car, position):
-		"""return True if the car is compatible with the board when fixed in postion for some orientation"""
-		self.array[position] = car
+	def is_compatible(self, square, position):
+		"""return True if the square is compatible with the board when fixed in postion for some orientation"""
+		self.array[position] = square
 		for i in range(4):
 			# each of the four orientations
-			car.rotate()
+			square.rotate()
 			if self.is_compatible_board():
 				return True
 		self.array[position] = None
@@ -86,21 +89,22 @@ class Board:
 	def is_compatible_board(self):
 		"""return True if the board is in a compatible configuration"""
 
-		for i,j in self.list_pos:
-			# compare all connections of the car in pos i,j
-			current_pos = (i,j)
-			current_car = self.array[i,j]
+		for i in range(self.shape[0]):
+			for j in range(self.shape[1]):
+				# compare all connections of the square in pos i,j
+				current_pos = (i,j)
+				current_square = self.array[i,j]
 
-			if current_car is not None: # is not necessary to check empty entries 
+				if current_square is not None: # is not necessary to check empty entries 
 
-				for (k, l), cardinal_dir in self.get_adjacent_indices(current_pos): # Northern, Southern, Easter and western neighbour
-					adjacent_car = self.array[k,l]
+					for (k, l), cardinal_dir in self.get_adjacent_indices(current_pos): # Northern, Southern, Easter and western neighbour
+						adjacent_square = self.array[k,l]
 
-					if adjacent_car is not None:
-						
-						if not current_car.compare(adjacent_car,cardinal_dir):
-							"""There is in incompatible conection"""
-							return False
+						if adjacent_square is not None:
+							
+							if not current_square.compare(adjacent_square,cardinal_dir):
+								"""There is in incompatible conection"""
+								return False
 		# conditions are satisfied
 		return True
 
@@ -116,11 +120,11 @@ class Board:
 			l1 = ""
 			l2 = ""
 			l3 = ""
-			for car in row:
-				if car is None:
+			for square in row:
+				if square is None:
 					sl = ["      "," None ", "      "]
 				else:
-					sl = car.string_list()
+					sl = square.string_list()
 				l1 += sl[0]
 				l2 += sl[1]
 				l3 += sl[2]
@@ -133,21 +137,22 @@ class Recursive:
 		#read csv puzzle
 		df = pd.read_csv(path, sep="\t")
 
-		# populate the cars
-		cars = [] 
+		# populate the squares
+		squares = [] 
 		for id, row in df.iterrows():
-			car = Car([row.N, row.W, row.S, row.E], id=str(id))
-			cars.append(car)
+			square = Square([row.N, row.W, row.S, row.E], id=str(id))
+			squares.append(square)
+		np.random.shuffle(squares)
 
 		#instantiate board
 		self.strategy = strategy
-		self.cars = set(cars) # populate cars
+		self.squares = set(squares) # populate squares
 		self.board = Board(shape)
 
-		self.counter = 0
+		self.counter = 0 # counts the number of recursive calls to measure performance
 
 	
-	def depth_search(self, depth = 0, cars_visited = set()):
+	def depth_search(self, depth = 0, squares_visited = set()):
 		self.counter += 1
 
 		if depth == len(self.strategy):
@@ -159,16 +164,10 @@ class Recursive:
 
 		current_pos = self.strategy[depth]
 
-		
+		for square in self.squares - squares_visited:
 
-		un_visited_cars = list(self.cars - cars_visited)
-
-		un_visited_cars  = [un_visited_cars[i] for i in np.random.permutation(len(un_visited_cars))]
-
-		for car in un_visited_cars:
-
-			if self.board.is_compatible(car, current_pos):
-				branch_has_future = self.depth_search(depth = depth+1,cars_visited = cars_visited.union(set([car])))
+			if self.board.is_compatible(square, current_pos):
+				branch_has_future = self.depth_search(depth = depth+1,squares_visited = squares_visited.union(set([square])))
 
 				if branch_has_future:
 					return True
@@ -187,11 +186,11 @@ suma = 0
 n=1
 for i in range(n):
 
-	r = Recursive(strategy,path = "carcomb - original.tsv", shape=(3,3))
+	r = Recursive(strategy,path = "data.tsv", shape=(3,3))
 
 	# run
 	r.depth_search()
-	# print(r.board)
+	print(r.board)
 
 	suma += r.counter
 
